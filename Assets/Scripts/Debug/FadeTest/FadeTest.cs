8782455510c6b0844d0161c172bf03a6c03214a9
siguma <you8jun5@outlook.jp>
2016-11-18 16:07:10 +0900

    fadeのデバッグ用シーン作成

---
+++ Assets/Scripts/Debug/FadeTest/FadeTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace debug.fadetest
+{
+    public class FadeTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            utility.fade.FadeManager.Init();
+        }
+
+        public void FadeIn()
+        {
+            utility.fade.Fade.FadeIn(1.0f, () => Debug.Log("fade in end"));
+        }
+
+        public void FadeOut()
+        {
+            utility.fade.Fade.FadeOut(1.0f, () => Debug.Log("fade out end"));
+        }
+    }
+}
