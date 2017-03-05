85b628d4ba3313b28cda942f61f73442ed4acd32
siguma <you8jun5@gmail.com>
2017-03-05 18:36:19 +0900

    ボタンの長押し判定を取れるTrigger作成

---
+++ Assets/Scripts/Debug/InputTest/InputTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UniRx;
+using utility.input;
+
+public class InputTest : MonoBehaviour {
+
+    private void Start() {
+        var test = gameObject.AddComponent<ObservableLongPointerDownTrigger>();
+        test.OnLongPointerDownAsObservable()
+            .Subscribe(_ => Debug.Log("長押しされた！！！"));
+    }
+}
