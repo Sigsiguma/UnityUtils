44685d1e7c756e39617b5a695fd4175d2413e3e4
Siguma <you8jun5@gmail.com>
2017-04-02 18:53:55 +0900

    InputをUniRxのTriggerのように拡張メソッドを追加

--- Assets/Scripts/Debug/FadeTest/FadeTest.cs
+++ Assets/Scripts/Debug/FadeTest/FadeTest.cs
@@ -1,22 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using utility.input;
 
-namespace debug.fadetest
-{
-    public class FadeTest : MonoBehaviour
-    {
-        private void Start()
-        {
+namespace debug.fadetest {
+    public class FadeTest : MonoBehaviour {
+        private void Start() {
             utility.fade.FadeManager.Init();
         }
 
-        public void FadeIn()
-        {
+        public void FadeIn() {
             utility.fade.Fade.FadeIn(1.0f, () => Debug.Log("fade in end"));
         }
 
-        public void FadeOut()
-        {
+        public void FadeOut() {
             utility.fade.Fade.FadeOut(1.0f, () => Debug.Log("fade out end"));
         }
     }
