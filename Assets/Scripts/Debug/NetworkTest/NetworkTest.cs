3672d2f9b637205dc6c3a65bd01fb04f64a90f84
Siguma <you8jun5@gmail.com>
2017-09-07 16:25:18 +0900

    HttpClientクラス、APIのインターフェイスの作成

---
+++ Assets/Scripts/Debug/NetworkTest/NetworkTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace network {
+    public class NetworkTest : MonoBehaviour {
+
+        private void Awake() {
+            MessagePack.Resolvers.CompositeResolver.RegisterAndSetAsDefault(
+                MessagePack.Resolvers.StandardResolver.Instance
+            );
+        }
+
+        private void Start() {
+            var httpClient = new HttpClient(this);
+
+            var api = new APITest();
+
+            var httpParam = new HttpParam();
+            httpParam.url_ = "hoge";
+            var req = new APITestRequest();
+            req.common = new CommonRequest();
+            req.common.userID = 1234;
+            req.request = 1;
+            httpParam.request = req;
+
+            httpClient.Request(api, httpParam);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Utility/Network/APITest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace network {
+    public class APITest : APIInterface {
+
+        public override byte[] PackRequest(RequestBase request) {
+            var req = request as APITestRequest;
+            return PackRequest<APITestRequest>(req);
+        }
+
+        public override ResponseBase UnPackResponse(byte[] data) {
+            return UnPackResponse<APITestResponse>(data);
+        }
+
+    }
+
+    [MessagePack.MessagePackObject]
+    public class APITestRequest : RequestBase {
+        [MessagePack.Key(1)]
+        public int request = 0;
+    }
+
+    [MessagePack.MessagePackObject]
+    public class APITestResponse : ResponseBase {
+        [MessagePack.Key(1)]
+        public int response = 0;
+    }
+}
